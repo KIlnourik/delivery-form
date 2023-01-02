4824de4a77b28ec343d9e37342593469665931a9
KIlnur <kilnur1988@rambler.ru>
2023-01-02 13:22:47 +0500

    Реализовал переключение форм в зависимости от вида доставки

---
+++ js/main.js
@@ -0,0 +1,2 @@
+import './modules/form-switch.js';
+
---
+++ js/modules/form-switch.js
@@ -0,0 +1,46 @@
+const deliveryForm = document.querySelector('.tabs-block__item-delivery');
+const pickUpFrom = document.querySelector('.tabs-block__pick-up');
+const deliveryTabs = document.querySelectorAll('.tab');
+
+deliveryForm.style.display = 'none';
+
+const formDisabler = (tabData) => {
+  switch (tabData) {
+    case 'pickup':
+      deliveryForm.style.display = 'none';
+      pickUpFrom.style.display = 'block';
+      break;
+    case 'delivery':
+      pickUpFrom.style.display = 'none';
+      deliveryForm.style.display = 'block';
+      break;
+  }
+};
+
+const tabsSwitch = (evt) => {
+  evt.preventDefault();
+  const targetTab = evt.target.closest('.tab');
+  deliveryTabs.forEach((tab) => {
+    if (!targetTab.classList.contains('active')) {
+      targetTab.classList.add('active');
+      if (tab.classList.contains('active')) {
+        tab.classList.remove('active');
+      }
+      formDisabler(targetTab.dataset.tab);
+    }
+  });
+  // deliveryTabs.forEach((tab) => {
+  //   if (!tab.classList.contains('active')) {
+  //     tab.classList.add('active');
+  //     formDisabler(tab.dataset.tab);
+  //   } else {
+  //     tab.classList.remove('active');
+  //   }
+  // });
+};
+
+deliveryTabs.forEach((tab) => {
+  tab.addEventListener('click', tabsSwitch);
+});
+
+
