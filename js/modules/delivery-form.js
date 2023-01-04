e64106bb85ce8aebb17f37154faa82378acaba2d
KIlnur <kilnur1988@rambler.ru>
2023-01-04 16:04:42 +0500

    Создал заготовки под валидацию форм. Сделал отключение поля карты в случае выбора наличных

--- js/modules/delivery-form.js
+++ js/modules/delivery-form.js
@@ -1,4 +1,9 @@
-const payTabs = document.querySelectorAll('.tabs-block__pick-up > .input-wrapper--payment-method > input');
+
+
+const pickUpBlock = document.querySelector('.tabs-block__pick-up');
+const payTabsWrapper = pickUpBlock.querySelector('.input-wrapper--payment-method');
+const payTabs = payTabsWrapper.querySelectorAll('input');
+
 const cardInputField = document.querySelector('.card');
 
 document.querySelector('#payment-card').checked = true;
@@ -6,26 +11,22 @@ document.querySelector('#payment-card').checked = true;
 const cardFieldDisable = (value) => {
   switch (value) {
     case 'cash':
-      cardInputField.style.display = 'none';
+      cardInputField.querySelectorAll('input').forEach((input) => input.setAttribute('disabled', 'disabled'));
       break;
     case 'card':
-      cardInputField.style.display = 'flex';
+      cardInputField.querySelectorAll('input').forEach((input) => input.removeAttribute('disabled', 'disabled'));
+      break;
   }
 };
 
 const payTabOnclickChange = (evt) => {
-  evt.preventDefault();
-  const targetInput = evt.target.closest('input');
-  targetInput.checked = true;
-  console.log(targetInput);
-  console.log(targetInput.checked);
-  console.log(targetInput.value);
-  cardFieldDisable(targetInput.value);
   payTabs.forEach((payTab) => {
     if (payTab.checked === true) {
       payTab.checked = false;
     }
   });
+  evt.target.checked = true;
+  cardFieldDisable(evt.target.value);
 };
 
 payTabs.forEach((tab) => tab.addEventListener('click', payTabOnclickChange));
